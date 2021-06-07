7bee5a008fed9a9e6f6096f88bc4853fd5c23a98
Nikolaos Kamzelas <nickbrick@users.noreply.github.com>
2021-06-07 22:07:04 +0300

    Add files via upload

--- hanoi/hanoi.js
+++ hanoi/hanoi.js
@@ -1,53 +1,64 @@
 
-dump = function(){
+dump = function () {
     towers.forEach(t => {
         console.log(t);
     });
     console.log(hand);
 }
 
-getTower = function(i){
+getTower = function (i) {
     return $(`.tower[data-i="${i}"]`);
 }
 
-getDisc = function(r){
+getDisc = function (r) {
     return $(`.disc[data-r="${r}"]`);
 }
 
-init = function(n){
-    towers=[[...Array(n).keys()].reverse(),[],[]];
+init = function (n) {
+    towers = [[...Array(n).keys()].reverse(), [], []];
     hand = null;
     for (let i = 0; i < n; i++) {
-        getTower(0).append(`<div data-r="${i}" class="disc" style="width:${i+1}0%;">${i}</div>`);
-        
+        getTower(0).append(`<div data-r="${i}" class="disc" style="width:${i + 1}0%;">${i}</div>`);
+
     }
 }
 
-lift = function(i){
-    if (hand == null){
-        if (towers[i].length > 0){
+lift = function (i) {
+    if (hand == null) {
+        if (towers[i].length > 0) {
             hand = towers[i].pop();
             getDisc(hand).addClass('lift');
         }
     }
 }
 
-drop = function(i){
+drop = function (i) {
     if (hand != null)
-        if (towers[i][towers[i].length - 1] > hand || towers[i].length == 0){
+        if (towers[i][towers[i].length - 1] > hand || towers[i].length == 0) {
             towers[i].push(hand);
             disc = getDisc(hand).detach();
             getTower(i).prepend(disc);
             disc.removeClass('lift');
             hand = null;
-            moves ++;
+            moves++;
         }
 }
 
-checkWin = function(){
-    if ((towers[1].length == n) || (towers[2].length == n)){
+act = function (i) {
+    if (moves == 0) t0 = performance.now();
+    if (hand == null)
+        lift(i);
+    else
+        drop(i);
+    checkWin();
+}
+
+checkWin = function () {
+    if ((towers[1].length == n) || (towers[2].length == n)) {
         t1 = performance.now();
-        alert(`Win. Moves: ${moves}/${2**n-1} Time: ${Math.round((t1-t0))/1000} s`);
-        window.location = window.location;
+        setTimeout(() => {
+            alert(`Win. Moves: ${moves}/${2 ** n - 1} Time: ${Math.round((t1 - t0)) / 1000} s`);
+            window.location = window.location;
+        }, 10);
     }
 }
